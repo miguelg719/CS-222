69cbf66da2fc9e9a3e1efab1b6780f01b39612c0
miguelg719 <36487034+miguelg719@users.noreply.github.com>
2018-04-04 09:28:42 -0400

    Update SinglyLinked.c

--- Class_Practice/SinglyLinked.c
+++ Class_Practice/SinglyLinked.c
@@ -7,48 +7,85 @@ typedef struct _node
 	struct _node* next;
 } node;
 
-void remove( node** headPointer, int value );
+void add( node** headPointer, int value );
+node* find( node* headPointer, int value );
+void Remove( node** headPointer, int value );
 void insert( node** headPointer, int value );
 void empty( node* head );
 
 int main()
 {
-	
-	
+  node* head = NULL;
+  add(&head, 5);
+  add( &head, 7 );
+  add( &head, 8 );
+  head = find( head, 7 );
+  printf("%d\n", head->data);
+
 	return 0;
 }
 
-void remove( node** headPointer, int value )
+void add( node** headPointer, int value )
 {
-	node head = headPointer;
-	
+  node* head = *headPointer;
+
+  node* box = (node*)malloc(sizeof(node));
+  box->data = value;
+  box->next = head;
+  *headPointer = box;
+
+}
+
+node* find( node* headPointer, int value )
+{
+  node* head = headPointer;
+
+  if( head->data == value )
+    return head;
+
+  while( head != NULL )
+  {
+    if( head->data == value )
+      return head;
+
+    head = head->next;
+  }
+
+  return NULL;
+
+}
+
+void Remove( node** headPointer, int value )
+{
+	node* head = *headPointer;
+
 	if( head == NULL )
 		return;
-	
+
 	if( head->data == value )
 	{
 		*headPointer = head->next;
 		free( head );
 	}
-	
+
 	while( head->next != NULL )
-	{	
+	{
 		if( head->next->data == value )
 		{
 			node* temp = head->next;
 			head->next = head->next->next;
 			free( temp );
 			return;
-		}	
-		
+		}
+
 		head = head->next;
 	}
 }
 
 void insert( node** headPointer, int value )
 {
-	node* head = headPointer;
-	
+	node* head = *headPointer;
+
 	if( (head == NULL) || (value < head->data) )
 	{
 		node* box = (node*)malloc(sizeof(node));
@@ -56,15 +93,15 @@ void insert( node** headPointer, int value )
 		box->next = head;
 		*headPointer = box;
 	}
-	
+
 	while( (head->next != NULL) && (head->next->data < value) )
-		head = head->next 
-		
+		head = head->next;
+
 		node* box = (node*)malloc(sizeof(node));
 		box->data = value;
 		box->next = head->next;
 		head->next = box;
-		return;		
+		return;
 }
 
 void empty( node* head )
