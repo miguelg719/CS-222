c5040a7b280ebc7ed6bc409b993eda2253eb5926
miguelg719 <36487034+miguelg719@users.noreply.github.com>
2018-04-13 12:58:12 -0400

    Add files via upload

---
+++ hex.c
@@ -0,0 +1,33 @@
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("What file do you want to open? ");
+	char filename[256];
+	scanf("%s", filename);
+	
+	int fd = open(filename, O_RDONLY);
+	
+	unsigned char buffer[16];
+	int bytes = 16;
+	int place = 0;
+	
+	while( bytes == 16 )
+	{	
+		bytes = read(fd, buffer, sizeof(unsigned char) * 16);
+		printf("0x%06x :", bytes);
+		place += bytes;
+		int i;
+		for( i = 0; i < bytes; i++ )
+			printf(" %02x", buffer[i]);
+		printf("\n");
+	}
+	
+	close(fd);
+	
+	return 0;
+}
